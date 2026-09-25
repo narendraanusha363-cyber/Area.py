68a5d7d59dda7507d81cb809c3a23cf1738c2e16
narendraanusha363-cyber <narendraanusha363@gmail.com>
2026-09-25 15:36:55 +0530

    Update Area.py

--- Area.py
+++ Area.py
@@ -3,3 +3,5 @@ length = float(input("Enter length: "))
 width = float(input("Enter width: "))
 area = length * width
 print(f"Area: {area:.2f} square units.")
+perimeter = 2 * (length + width)
+print(f"The perimeter of rectangle is: {perimeter} units)
